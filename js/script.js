bb7865f8110c56ef6a14efe3518013f6e217f7e5
Lera <142676297+UlianaPostevka06@users.noreply.github.com>
2025-03-01 18:21:55 +0200

    щось зробила но мало

---
+++ js/script.js
@@ -0,0 +1,6 @@
+let level_toggle = document.querySelectorAll('.level-toggle')
+let level__description_block = document.querySelector('.level__description-block')
+
+level_toggle.addEventListener('click', function(){
+    level__description_block.style.display == 'none': level__description_block.style.display == 'block';
+})
